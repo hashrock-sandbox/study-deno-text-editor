6959dbdc2846bf30d8a86bada6fd6cb9a8e2967d
hashrock <hashrock@users.noreply.github.com>
2020-03-15 02:10:49 +0900

    is this world's first deno editor? idk :)

---
+++ index.ts
@@ -0,0 +1,71 @@
+import { WebView } from "https://deno.land/x/webview/mod.ts";
+
+const webview1 = new WebView({
+  title: "Deno TextEditor",
+  url: `data:text/html,
+    <html>
+    <head>
+    <style>
+      html, body{
+        height: 300px;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+      }
+      textarea{
+        background: #333;
+        flex: 1;
+        color: white;
+        width: 300px;
+      }
+      button{
+        width: 100px;
+      }
+    </style>
+    </head>
+    <body>
+    <textarea id="editor"></textarea>
+    <div>
+      <button onclick="save()">Save</button>
+      <span id="msg">loaded</span>
+    </div>
+    <script>
+    var el = document.getElementById("msg")
+    var textarea = document.getElementById("editor")
+
+    function load(){
+      var xmlHttpRequest = new XMLHttpRequest();
+      xmlHttpRequest.onreadystatechange = function(){
+          if(this.readyState == 4 && this.status == 200){
+              var now = new Date();
+              el.innerText = "loaded: " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+              editor.value = this.responseText;
+          }
+      }
+      xmlHttpRequest.open('GET','http://127.0.0.1:8899/editor_load',true);
+      xmlHttpRequest.send();
+    }
+
+    function save(){
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", 'http://127.0.0.1:8899/editor', true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      xhr.onreadystatechange = function() { 
+          if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            var now = new Date();
+            el.innerText = "saved: " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
+          }
+      }
+      xhr.send(textarea.value);
+    }
+    load();
+    </script>
+    </body>
+    </html>
+    `,
+  width: 300,
+  height: 300
+});
+
+while (webview1.step()) {
+}
---
+++ server.ts
@@ -0,0 +1,24 @@
+import { createRouter } from "https://servestjs.org/@v0.35.0/router.ts";
+const router = createRouter();
+router.post("/editor", async req => {
+  const form = await req.body?.text();
+  const encoder = new TextEncoder();
+  Deno.writeFileSync("out.txt", encoder.encode(form));
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/plain"
+    }),
+    body: "saved"
+  });
+});
+router.get("/editor_load", async req => {
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/plain"
+    }),
+    body: new TextDecoder().decode(Deno.readFileSync("out.txt"))
+  });
+});
+router.listen(":8899");
