4f5e18ccdb107a88abe9575ddbcc7ec90d19d317
hashrock <hashrock@users.noreply.github.com>
2020-05-20 11:11:16 +0900

    update servest

--- server.ts
+++ server.ts
@@ -1,26 +1,26 @@
-import { createRouter } from "https://servestjs.org/@v0.35.0/router.ts";
-const router = createRouter();
-router.post("/editor", async req => {
-  const form = await req.body?.text();
+import { createApp } from "https://servestjs.org/@v1.0.0/mod.ts";
+const router = createApp();
+router.post("/editor", async (req) => {
+  const form = await req.text();
   const encoder = new TextEncoder();
   Deno.writeFileSync("out.txt", encoder.encode(form));
   await req.respond({
     status: 200,
     headers: new Headers({
       "content-type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
     }),
-    body: "saved"
+    body: "saved",
   });
 });
-router.get("/editor_load", async req => {
+router.get("/editor_load", async (req) => {
   await req.respond({
     status: 200,
     headers: new Headers({
       "content-type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
     }),
-    body: new TextDecoder().decode(Deno.readFileSync("out.txt"))
+    body: new TextDecoder().decode(Deno.readFileSync("out.txt")),
   });
 });
-router.listen(":8899");
+router.listen({ port: 8899 });
